6ce7711866203ccaf8ca7cd02fd3999b7e976ac4
amvrosiadispavlos-dot <amvrosiadispavlos@gmail.com>
2025-10-31 01:07:58 +0300

    Add files via upload

---
+++ 1.13.py
@@ -0,0 +1,5 @@
+number=int(input("Введите целое число:"))
+next_number=number+1
+prev_number=number-1
+print("Следующее за числом",number,"число-",next_number)
+print("Для числа",number,"предыдущее число-",prev_number)
